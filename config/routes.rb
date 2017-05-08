ae31609aa21fa69b70bb855e50aaeacc40f4a3dd
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-08 14:06:17 -0500

    Add subscribe customer functionality

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   get 'customers/show', to: 'customers#show'
   get 'plans/new'
   post 'plans/create'
+  get 'plans/newsubscribe'
+  post 'plans/subscribe', to: 'plans#subscribe'
 end
